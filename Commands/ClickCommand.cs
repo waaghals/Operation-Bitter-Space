6e76e7d251940a3bd176ec1beea0df9e9a13895a
Patrick Berenschot <patrick.stroef@gmail.com>
2013-10-15 19:25:38 +0200

    Added ClickCommand, Buttons will no only trigger mouse over when valid in ICommand

---
+++ Commands/ClickCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+namespace Hexxagon.Commands
+{
+
+    internal class ClickCommand : ICommand
+    {
+        private Cell cell;
+
+        public ClickCommand(Cell c) {
+            cell = c;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return cell.Hex.Available();
+        }
+
+        public void Execute(object parameter) {
+            //Doe iets
+        }
+    }
+}
--- ViewModels/Cell.cs
+++ ViewModels/Cell.cs
@@ -1,22 +1,27 @@
-﻿using Hexxagon.Helpers;
+﻿using Hexxagon.Commands;
+using Hexxagon.Helpers;
 using Hexxagon.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Hexxagon
 {
     public class Cell
     {
+        public ICommand ClickCommand { get; set; }
         public Hexagon Hex { get; set; }
         public Brush Gradient { get; set; }
 
         public Cell(Hexagon h)
         {
             Hex = h;
+            ClickCommand = new ClickCommand(this);
+
             if (Hex.Available())
             {
                 Gradient = CellGradient.FromHue(0, 0.0);
@@ -24,11 +29,11 @@ namespace Hexxagon
             else if (Hex.IsOwned())
             {
                 PlayerHexagon playerHex = (PlayerHexagon)Hex;
-                Gradient = CellGradient.FromHue(playerHex.Owner.Hue, 0.5);
+                Gradient = CellGradient.FromHue(playerHex.Owner.Hue, 0.7);
             }
             else
             {
-                Gradient = CellGradient.FromHue(0, 0.05);
+                Gradient = CellGradient.FromHue(0, 0.15);
             }
         }
     }
